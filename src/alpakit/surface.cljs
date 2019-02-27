c6fd1008f2b53029e79409d1a352e5d2031860ec
vitalipe <vitali.pe@gmail.com>
2019-02-27 14:24:29 +0200

    surface skeleton

--- src/alpakit/surface.cljs
+++ src/alpakit/surface.cljs
@@ -1,22 +1,23 @@
 (ns alpakit.surface
   (:require
-    [alpakit.widget :refer-macros [widget]]
+    [alpakit.widget :refer-macros [defwidget]]
     [alpakit.props :as props]))
 
 
 
 
-(def surface
-  (widget
-    "a simple building block for more complex widgets"
+(defwidget surface
+  "a simple building block for more complex widgets"
 
-    :props {background  {:default ""  :spec props/css-background}
-            border      {:default ""  :spec props/css-border}
-            padding     {:default ""  :spec props/css-size}
+  :props {background  {:default ""  :spec props/css-background}
+          border      {:default ""  :spec props/css-border}
+          padding     {:default ""  :spec props/css-size}
 
-            hidden?     {:default false :spec boolean?}
-            disabled?   {:default false :spec boolean?}
-            on-click    {:default nil   :spec props/on-click-fn}
+          hidden?     {:default false :spec boolean?}
+          disabled?   {:default false :spec boolean?}
+          on-click    {:default nil   :spec props/on-click-fn}
 
-            -attr       {:default {}     :spec props/html-attr-map}
-            -style      {:default {}     :spec props/css-style-map}}))
+          -attr       {:default {}     :spec props/html-attr-map}
+          -style      {:default {}     :spec props/css-style-map}})
+
+  ;; TODO 
